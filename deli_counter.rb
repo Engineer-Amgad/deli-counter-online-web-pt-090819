d23faebe60dc155e9c2ba4158aa3a35de09b3e1f
Amgad Malik <amgadmalik@yahoo.com>
2019-09-19 02:50:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,26 +2,20 @@
 katz_deli = []
 
 def line(katz_deli)
-  
   if katz_deli.length < 1
     puts "The line is currently empty." 
   else
-    current_line = []
     counter = 0 
     katz_deli.each do |el|
-      #current_line << "#{katz_deli.index(el) + 1}. #{el}"
       katz_deli[counter] = "#{katz_deli.index(el) + 1}. #{el}"
       counter += 1 
     end
-    #puts "The line is currently: #{current_line.join(" ")}"
     puts "The line is currently: #{katz_deli.join(" ")}"
   end
-  
 end 
 
 def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
-  
 end 
 
