f0785fd6c8f9b4526e1db81b05132b52f3fa5415
Amgad Malik <amgadmalik@yahoo.com>
2019-09-19 02:55:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.length < 1 
-    puts "The line is empty"
+    puts "There is nobody waiting to be served!"
   else 
     puts "Now serving #{katz_deli.shift}."
   end 
